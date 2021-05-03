a82911c441c5a3d090038bd462646e5e9bcfe542
pugetoken <pugetoken@gmail.com>
2021-05-03 18:14:18 +0800

    commit

---
+++ src/layout/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer style="background-color:black">
+   <p style="text-align:center;color:white;margin-bottom:0 !important">&#169; 2021 Puge.io. All Rights Reserved</p>
+  </footer>
+</template>
